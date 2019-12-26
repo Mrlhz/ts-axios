45d7b36b2eb830f6dd6c996f0ea1f0215c5484cc
mrlhz <mr__lhz@163.com>
2019-12-27 01:39:56 +0800

    feat: Process response header

--- src/helper/headers.ts
+++ src/helper/headers.ts
@@ -34,3 +34,28 @@ export function processHeaders(headers: any, data: any) {
 
   return headers
 }
+
+/**
+ * @description header string to object
+ * @export
+ * @param {string} headers
+ * @returns {object}
+ */
+export function parseHeaders(headers: string) {
+  let parsed = Object.create(null)
+  if (!headers) return parsed
+
+  headers.split('\r\n').forEach(line => {
+    let [key, value] = line.split(':')
+    key = key.trim().toLowerCase()
+    if (!key) return
+
+    if (value) {
+      value = value.trim()
+    }
+
+    parsed[key] = value
+  })
+
+  return parsed
+}
